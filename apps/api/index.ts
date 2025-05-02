d1fe941574cfe75e2b649e1d42853c148e1cd414
komalkbairwa18 <komalbairwa.1804@gmail.com>
2025-05-02 18:37:06 +0530

    api changes-v1

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import express from "express"
+import { authMiddleware } from "./middleware";
+
+const app = express();
+
+app.post("/api/v1/website",authMiddleware, (req,res) => {
+
+    const userId = req.userId;
+
+})
+
+app.get("/api/v1/website/status", authMiddleware, (req,res) =>{
+
+})
+
+app.get("/api/v1/websites", authMiddleware,(req,res) => {
+
+})
+
+app.delete("/api/v1/website/:websiteId", authMiddleware ,(req,res) => {
+    
+})
+
+app.listen(3000);
